1e4f6010204f48fc7992565f88bd5bc9a1864c24
Mopilani - Nexapros Softwares <mopilani1@gmail.com>
2023-10-17 23:06:10 +0200

    Add converters and object fillers

--- lib/models/models.dart
+++ lib/models/models.dart
@@ -1,14 +1,14 @@
 class Block {
-  Block(
-    this.minedBy,
-    this.transactions,
-    this.height,
-    this.difficulty,
-    this.hash,
-    this.previousHash,
-    this.nonce,
-    this.timestamp,
-  );
+  Block({
+    required this.minedBy,
+    required this.transactions,
+    required this.height,
+    required this.difficulty,
+    required this.hash,
+    required this.previousHash,
+    required this.nonce,
+    required this.timestamp,
+  });
 
   String minedBy;
   String transactions;
@@ -18,6 +18,32 @@ class Block {
   String previousHash;
   String nonce;
   int timestamp;
+
+  Map<String, dynamic> asMap() {
+    return {
+      'minedBy': minedBy,
+      'transactions': transactions,
+      'height': height,
+      'difficulty': difficulty,
+      'hash': hash,
+      'previousHash': previousHash,
+      'nonce': nonce,
+      'timestamp': timestamp,
+    };
+  }
+
+  static Block fromMap(Map<String, dynamic> data) {
+    return Block(
+      minedBy: data['minedBy'],
+      transactions: data['transactions'],
+      height: data['height'],
+      difficulty: data['difficulty'],
+      hash: data['hash'],
+      previousHash: data['previousHash'],
+      nonce: data['nonce'],
+      timestamp: data['timestamp'],
+    );
+  }
 }
 
 class Peer {
@@ -30,6 +56,22 @@ class Peer {
   String ip;
   int port;
   int lastSeen;
+
+  Map<String, dynamic> asMap() {
+    return {
+      'ip': ip,
+      'port': port,
+      'lastSeen': lastSeen,
+    };
+  }
+
+  static Peer fromMap(Map<String, dynamic> data) {
+    return Peer(
+      data['ip'],
+      data['port'],
+      data['lastSeen'],
+    );
+  }
 }
 
 class Ping {
@@ -42,17 +84,33 @@ class Ping {
   int blockHeight;
   int peerCount;
   bool isMiner;
+
+  Map<String, dynamic> asMap() {
+    return {
+      'blockHeight': blockHeight,
+      'peerCount': peerCount,
+      'isMiner': isMiner,
+    };
+  }
+
+  static Ping fromMap(Map<String, dynamic> data) {
+    return Ping(
+      data['blockHeight'],
+      data['peerCount'],
+      data['isMiner'],
+    );
+  }
 }
 
 class Transaction {
-  Transaction(
-    this.hash,
-    this.sender,
-    this.receiver,
-    this.signature,
-    this.timestamp,
-    this.amount,
-  );
+  Transaction({
+    required this.hash,
+    required this.sender,
+    required this.receiver,
+    required this.signature,
+    required this.timestamp,
+    required this.amount,
+  });
 
   String hash;
   String sender;
@@ -60,4 +118,26 @@ class Transaction {
   String signature;
   int timestamp;
   double amount;
+
+  Map<String, dynamic> asMap() {
+    return {
+      'hash': hash,
+      'sender': sender,
+      'receiver': receiver,
+      'signature': signature,
+      'timestamp': timestamp,
+      'amount': amount,
+    };
+  }
+
+  static Transaction fromMap(Map<String, dynamic> data) {
+    return Transaction(
+      hash: data['hash'],
+      sender: data['sender'],
+      receiver: data['receiver'],
+      signature: data['signature'],
+      timestamp: data['timestamp'],
+      amount: data['amount'],
+    );
+  }
 }
