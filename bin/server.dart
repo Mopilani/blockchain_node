bdf593dafe5d935b433e391a27f27f928368f10a
Mopilani - Nexapros Softwares <mopilani1@gmail.com>
2023-10-18 19:15:31 +0200

    Init Mongo service on start

--- bin/server.dart
+++ bin/server.dart
@@ -3,12 +3,16 @@ import 'dart:typed_data';
 
 import 'package:blockchain_node/models/blockchain.dart';
 import 'package:blockchain_node/router.dart';
+import 'package:blockchain_node/services/mongo_db.dart';
 import 'package:blockchain_node/shared.dart';
 import 'package:shelf/shelf.dart';
 import 'package:shelf/shelf_io.dart';
 
-
 void main(List<String> args) async {
+  service = MongoService(dbURI: 'mongodb://localhost:27017/batarina');
+  await service.initial();
+  db = service.db;
+
   blockchain = Blockchain();
   // Use any available host or container IP (usually `0.0.0.0`).
   final ip = InternetAddress.fromRawAddress(Uint8List.fromList([0, 0, 0, 0]));
