c3beea9b18297006de3b37eb4aaa7921dbbcbd79
Mopilani - Nexapros Softwares <mopilani1@gmail.com>
2023-10-18 10:41:11 +0200

    Add Body Keys-values

---
+++ lib/utils/body_kvs.dart
@@ -0,0 +1,9 @@
+class DbBodykvs {
+  static const String exception = 'exp'; //
+  static const String message = 'msg'; //
+  static const String entries = 'nts'; //
+  static const String writeResult = 'wrr'; //
+  static const String result = 'rsl'; //
+  static const String error = 'err'; //
+  static const String data = 'dta'; //
+}
