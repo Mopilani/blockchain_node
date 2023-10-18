3a6c11b4f90ed3e97149bfac604bafca6f46b1bf
Mopilani - Nexapros Softwares <mopilani1@gmail.com>
2023-10-18 10:41:51 +0200

    Add some endpoints

--- bin/server.dart
+++ bin/server.dart
@@ -1,5 +1,7 @@
 import 'dart:io';
 
+import 'package:blockchain_node/models/blockchain.dart';
+import 'package:mongo_dart/mongo_dart.dart';
 import 'package:shelf/shelf.dart';
 import 'package:shelf/shelf_io.dart';
 import 'package:shelf_router/shelf_router.dart';
@@ -9,9 +11,21 @@ import 'package:shelf_web_socket/shelf_web_socket.dart' as sws;
 // Configure routes.
 final _router = Router()
   ..all('/', sws.webSocketHandler)
+  ..get('/mine', _mine)
+  ..post('/transactions/new', _newTransaction)
+  ..get('/chain', _chain)
   ..get('/echo/<message>', _echoHandler);
 
-Response _rootHandler(Request req) {
+Response _mine(Request req) {
+  return Response.ok('Hello, World!\n');
+}
+
+Response _newTransaction(Request req) {
+  return Response.ok('Hello, World!\n');
+}
+
+Response _chain(Request req) {
+  
   return Response.ok('Hello, World!\n');
 }
 
@@ -23,6 +37,11 @@ Response _echoHandler(Request request) {
 void main(List<String> args) async {
   // Use any available host or container IP (usually `0.0.0.0`).
   final ip = InternetAddress.anyIPv4;
+
+  String nodeIdentifier = Uuid().v4().replaceAll('-', '');
+
+// # Instantiate the Blockchain
+  var blockchain = Blockchain();
 
   // Configure a pipeline that logs requests.
   final handler = Pipeline().addMiddleware(logRequests()).addHandler(_router);
