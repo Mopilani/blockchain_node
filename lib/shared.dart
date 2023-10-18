078ee635e6c7c1d1f3cbc09be3b07fd2c7f9a1ef
Mopilani - Nexapros Softwares <mopilani1@gmail.com>
2023-10-18 19:16:00 +0200

    Share mongo service and db

--- lib/shared.dart
+++ lib/shared.dart
@@ -1,7 +1,11 @@
 import 'package:blockchain_node/models/blockchain.dart';
-import 'package:uuid/uuid.dart';
+import 'package:blockchain_node/services/mongo_db.dart';
+import 'package:mongo_dart/mongo_dart.dart';
 
 String nodeIdentifier = Uuid().v4().replaceAll('-', '');
 
 // # Instantiate the Blockchain
 late Blockchain blockchain;
+
+late Db db;
+late MongoService service;
